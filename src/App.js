cae4133f5d11109137265fe93be8e1c16f5bef6c
Dennis Xiao <dennisboys@gmail.com>
2020-01-11 13:49:31 +1000

    add code splitting example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
+const Warning = React.lazy(() => import('./Warning'))
+
 class App extends React.Component {
 	state = {
 		count: 0
@@ -14,17 +16,31 @@ class App extends React.Component {
 		this.setState(previousState => ({ count: previousState.count - 1 }))
 	}
 
+	renderWarning = () => {
+		const { count } = this.state
+
+		return count >= 3 
+			?	<React.Suspense fallback={null}>
+					<Warning />
+				</React.Suspense> 
+			: null
+	}
+
 	render() {
 		const { count } = this.state
 
 		return (
 			<div>
 				<h1>Hello World!!</h1>
+
 				<h2 className={count >= 3 ? 'warning' : null}>
 					Count: {this.state.count}
 				</h2>
+
 				<button onClick={this.increment}>+</button>
 				<button onClick={this.decrement}>-</button>
+
+				{this.renderWarning()}
 			</div>
 		)
 	}
