c90585d50283457467dd540ce496ddb3c498f092
Dennis Xiao <dennisboys@gmail.com>
2020-01-11 11:54:14 +1000

    add react-hot-loader to keep component state during development

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
 	state = {
 		count: 0
 	}
 
+	increment = () => {
+		this.setState(previousState => ({ count: previousState.count + 1 }))
+	}
+
+	decrement = () => {
+		this.setState(previousState => ({ count: previousState.count - 1 }))
+	}
+
 	render() {
+		const { count } = this.state
+
 		return (
 			<div>
-				<h1>Hello World!</h1>
-				<h2>Count: {this.state.count}</h2>
-				<button onClick={() => this.setState(previousState => ({ count: previousState.count + 1}))}>+</button>
-				<button onClick={() => this.setState(previousState => ({ count: previousState.count - 1}))}>-</button>
+				<h1>Hello World!!</h1>
+				<h2 className={count >= 3 ? 'warning' : null}>
+					Count: {this.state.count}
+				</h2>
+				<button onClick={this.increment}>+</button>
+				<button onClick={this.decrement}>-</button>
 			</div>
 		)
 	}
 }
 
-export default App
+export default hot(module)(App)
